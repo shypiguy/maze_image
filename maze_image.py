02fa8b104bf8940c3c1e1d408c6bffec2f94a659
Bill Jones <bill.jones.data.wrangler@gmail.com>
2026-04-11 15:32:10 -0500

    logging changes and bg hue changes

--- maze_image.py
+++ maze_image.py
@@ -1102,10 +1102,12 @@ else:
 print ("done")
 # determine the right hue for background images
 im_hue_center = im_phav[0]
+bg_hue = im_hue_center
 if im_hue_center > 128:
-    bg_hue = im_hue_center - 128
+    bg_hue = im_hue_center - 107
 else:
-    bg_hue = im_hue_center + 128
+    bg_hue = im_hue_center + 107
+
 logger.info("image hue center was %s, bg hue will be %s", im_phav,bg_hue) 
 
 # cut and size the background images
