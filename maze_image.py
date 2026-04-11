7864d7145c82b3da40b8db8d93f92ed3b7082574
Bill Jones <bill.jones.data.wrangler@gmail.com>
2026-04-11 15:40:03 -0500

    alter logging from info to debug

--- maze_image.py
+++ maze_image.py
@@ -1014,7 +1014,7 @@ for start_point in distance_list:
                     
                 max_dist = end_point[2]
 logger.info("longest path identifed (%s cells)", max_dist)
-logger.info('solution_list = %s', solution_list)
+logger.debug('solution_list = %s', solution_list)
 
 # build the path
 path=[[0 for col in range(width)] for row in range(height)]                
